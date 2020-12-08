71d51f0730a43b325747aa09a666b3698d626248
Logan Thirion <thirionlogan@gmail.com>
2020-12-08 14:14:45 -0600

    implement delete email

--- src/components/EmailDetails.js
+++ src/components/EmailDetails.js
@@ -2,16 +2,28 @@ import React, { useEffect, useState } from 'react';
 import { Redirect } from 'react-router-dom';
 const EmailDetails = ({ selectedEmailId }) => {
   const [email, setEmail] = useState({});
+  const [emailDeleted, setEmaildeleted] = useState(false);
 
   useEffect(() => {
     fetch(`http://localhost:3001/emails/${selectedEmailId}`)
       .then((response) => response.json())
       .then((data) => setEmail(data));
-  });
+  }, [selectedEmailId]);
+
+  const deleteEmail = () => {
+    fetch(`http://localhost:3001/emails/${selectedEmailId}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setEmaildeleted(data.status === 'success'));
+  };
 
   return (
     <>
-      {!selectedEmailId ? <Redirect to='/' /> : null}
+      {!selectedEmailId || emailDeleted ? <Redirect to='/' /> : null}
       {email ? (
         <div>
           <p>{`From: ${email.sender}`}</p>
@@ -19,6 +31,7 @@ const EmailDetails = ({ selectedEmailId }) => {
           <p>{`On: ${new Date(email.date).toDateString()}`}</p>
           <p>{`Subject: ${email.subject}`}</p>
           <p>{email.message}</p>
+          <button onClick={deleteEmail}>Delete</button>
         </div>
       ) : null}
     </>
